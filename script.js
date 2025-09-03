5da09cc5d5a43d2a239c5df20cac01de6557887a
NenadN22 <tonbe22@gmail.com>
2025-09-03 21:14:51 +0200

    Make function to check is combination winnable

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function Gameboard () {
     const getBoard = () => board;
 
     function addMark (index,mark) {
-        board[index] = " ";
-        board[index] = mark;
+        if(board[index] ===" ") {
+            board[index] = mark
+        } else {
+            console.log("Cant Place Here")
+        }
 
     }
-    addMark(2,'X')
      function availableCells(arr) {
         let Cells = []
         let element = " "
@@ -19,17 +21,25 @@ function Gameboard () {
             Cells.push(idx)
             idx = arr.indexOf(element, idx  + 1);
         }
-        
-        console.log(Cells) 
+        if(element !== " ") {
+            console.log("Cant place here")
+        } 
+       return(Cells) 
 
     }
+    function PlayersCombinations (arr) {
+        return arr.reduce()
+    }
+ 
     availableCells(getBoard())
     console.log(getBoard())
-    return {getBoard,addMark}
+    return {getBoard,addMark,availableCells}
+    
 
 }
 
 function Gameflow() {
+    const gameboard = Gameboard()
     function Player(name,mark) {
         return {
             name: name,
@@ -44,8 +54,29 @@ function Gameflow() {
         activePlayer = activePlayer  === Players[0] ? Players[1] : Players[0];
         return activePlayer
     }
+    function winnerCombinations() {
+        let combinations = [
+            [0,1,2],[0,3,6],[0,4,8],[1,4,7],
+            [2,5,8],[3,4,5],[6,3,0],[6,4,2],
+            [2,4,6],[7,4,1],[8,5,2],[8,4,0]
+        ]
+    }
+    function PlayRound (index) {
+        gameboard.addMark(index,activePlayer.mark)
+        console.log(gameboard.availableCells(gameboard.getBoard()))
+        
+        switchPlayersTurn()
+        console.log(gameboard.getBoard())
+    }
+    PlayRound(1)
     console.log(switchPlayersTurn())
-    return {switchPlayersTurn}
-}                  
-Gameflow()
-Gameboard()
+
+    return {switchPlayersTurn,PlayRound}
+}
+let game = Gameflow()
+game.PlayRound(1)
+game.PlayRound(2)
+game.PlayRound(3)
+     
+
+

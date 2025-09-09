f613d585e98fcd8ef433e75d94d163629d034cdf
NenadN22 <tonbe22@gmail.com>
2025-09-09 20:04:45 +0200

    - Implemented PlayersCombination() inside Gameboard to track player moves by index - Added winningCombinations array with all possible win scenarios - Created checkWinner() to verify if PlayerOne or PlayerTwo meets a winning condition - Integrated SwitchPlayers() function to alternate turns between players - Tested game flow with sample moves, logging board state and checking for winners after each move - Console logs added for debugging and validation of game logic

--- script.js
+++ script.js
@@ -10,11 +10,28 @@ function Gameboard () {
         
     }
     let getBoard = () => board
-    return {addMark,getBoard}
+    function PlayersCombination() {
+        let PlayerOneCombination = []
+        let PlayerTwoCombination = []
+        for(let i = 0; i < board.length; i++) {
+            if(board[i] === 'X') {
+                PlayerOneCombination.push(i)
+                
+            }else if (board[i] === 'O') {
+                PlayerTwoCombination.push(i)
+            }
+            
+        }
+        return{PlayerOneCombination,PlayerTwoCombination}
+    }
+    PlayersCombination()
+ 
+    return {addMark,getBoard,PlayersCombination}
 }
 
 function gameFlow () {
     let board = Gameboard()
+    let combinations = board.PlayersCombination
     function Players (name,mark)  {
         return {
             name: name,
@@ -23,11 +40,51 @@ function gameFlow () {
         
 
     }
+    const winningCombinations = [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]  
+    ];
+    function checkWinner() {
+        for(let winning of winningCombinations) {
+            if(winning.every(element => board.PlayerOneCombination.includes(element))) {
+                console.log("Player One Win")
+            } else if (winning.every(element => board.PlayerTwoCombination.includes(element))) {
+                console.log('Player Two Win')
+            }
+        }
+
+    }
     let PlayerOne = Players('PlayerOne', 'X');
     let PlayerTwo = Players('PlayerTwo', 'O');
     const players = [PlayerOne,PlayerTwo]
+    let activePlayer = players[0]
+    function SwitchPlayers () {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+        return activePlayer
+    }
+  
+    
     console.log(players[1])
-    let addMark = Gameboard()
-    console.log(board.addMark(1,PlayerOne.mark))
+    console.log(board.addMark(1,activePlayer.mark))
+    checkWinner()
+    SwitchPlayers()
+    console.log(board.addMark(5,activePlayer.mark))
+    checkWinner()
+    SwitchPlayers()
+    console.log(board.addMark(3,activePlayer.mark))
+    checkWinner()
+    SwitchPlayers()
+    console.log(board.addMark(4,activePlayer.mark))
+    checkWinner()
+        SwitchPlayers()
+    console.log(board.addMark(2,activePlayer.mark))
+    checkWinner()
+
 }
 gameFlow()

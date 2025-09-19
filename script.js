2ea748ce3be2977d325d2dd36ca8860ab92b1d5e
NenadN22 <tonbe22@gmail.com>
2025-09-19 22:04:19 +0200

    - Extended Gameboard with addMark() to prevent overwriting filled cells - Added getBoard() to return the current board state - Implemented winningCombinations array in GameFlow for win condition checks - Created checkWinner() function to track player moves (X and O) by index - Integrated addMark() and checkWinner() calls into GameFlow for testing - Added player switch mechanism but not yet fully wired into round flow

--- script.js
+++ script.js
@@ -3,23 +3,62 @@ function Gameboard () {
         " "," "," ",
         " "," "," "
     ]
-    
+
+    function addMark(index,mark) {
+        if(board[index] === " ") {
+            board[index] = mark
+        } else {
+            console.log("You cant place mark there")
+        }
+        return board;
+    }
+    let getBoard = () => board;
+    return {
+        getBoard,
+        addMark
+    }
 }
 function GameFlow () {
+   const game = Gameboard()
     function Player (name,mark) {
         return {
             name: name,
             mark: mark
         }
     }
+    let winningCombinations = [[0,1,2],[0,4,8],[0,3,6]
+[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
     let Player1 = Player('Player1','X');
     let Player2 = Player('Player2', 'O');
     let Players = [Player1,Player2];
     let activePlayer = Players[0]
     function switchPlayers () {
         activePlayer = activePlayer  === Players[0] ? Players[1] : Players[0]
-        return activePlayer
     }
+
+    function checkWinner() {
+        let board = game.getBoard()
+        let PlayerOneCombination = []
+        let PlayerTwoCombination = []
+        for(i = 0; i < board.length; i++) {
+            if(board[i] === 'X') {
+                PlayerOneCombination.push(i)
+                return PlayerOneCombination
+            } else if (board[i] === 'O') {
+                PlayerTwoCombination.push(i)
+                return PlayerTwoCombination
+            }
+            
+            
+
+        }
+        
+    }
+    game.addMark(0, activePlayer.mark)
+    checkWinner()
     
 }
+
 GameFlow()
+
+
